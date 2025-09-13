3818c5b2b5322dda8463ccad07d992b61554e32f
ʍɵ‌ʀꜱᴛʀɪ𒉽 <morstrixe@gmail.com>
2025-09-13 15:58:40 +0300

    feat: add 1mag.png under magazine info box

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     const monsterButton = document.getElementById('monster-button');
-    const monsterImage = monsterButton.querySelector('.monster-image');
-    
-    // Удаляем старую логику переключения изображений, так как они теперь статичны.
     
     monsterButton.addEventListener('click', () => {
-        // Добавляем эффект нажатия
         monsterButton.classList.add('is-pressed');
         
-        // Убираем эффект через 100 мс
         setTimeout(() => {
             monsterButton.classList.remove('is-pressed');
         }, 100);
