09fdf41866e54446f20484a5129f8a1f35db7014
ʍɵ‌ʀᔕᴛʀɪ𒉽 <morstrix@proton.me>
2025-09-23 18:44:56 +0300

    feat: replace loading page with menu and change buttons to folders

--- script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const monsterButton = document.getElementById('monster-button');
-    
-    // Animate the button using CSS :active pseudo-class instead of JS
-});
